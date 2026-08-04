effaaedfca573379522ca760abcaf121478cc5ff
anggierians21 <anggieriansetiawan21@gmail.com>
2026-08-04 17:44:02 +0800

    Create context.py

---
+++ runtime/context.py
@@ -0,0 +1,21 @@
+"""
+Runtime Context Provider
+
+Responsibilities:
+
+Provide runtime context to every Flora Agent.
+
+Future Agents:
+
+Research
+
+Strategy
+
+Writer
+
+Designer
+
+Publisher
+
+Analytics
+"""
