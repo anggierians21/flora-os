790552fd45445856d9a3fc8d953565e64060f55f
anggierians21 <anggieriansetiawan21@gmail.com>
2026-08-04 17:41:16 +0800

    Create loader.py

---
+++ runtime/loader.py
@@ -0,0 +1,18 @@
+"""
+Flora Runtime Loader
+
+Responsibilities:
+
+- Read every markdown file under knowledge/
+- Preserve folder hierarchy
+- Ignore hidden files
+- Return structured document list
+
+Future:
+
+load_all()
+
+load_folder()
+
+load_file()
+"""
